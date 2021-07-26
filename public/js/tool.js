40f49a012b758023d8b8263a79a50e28d6e52027
shikharpanwar4 <shikharpanwar8888@gmail.com>
2021-07-26 21:07:35 +0530

    added script for heroku

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("https://pepboard.herokuapp.com/");
+const socket = io.connect("https://realtime-whiteboard-app.herokuapp.com/");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
